f4edfb997f046fdc4950f6f38ecc9384ae0342fb
maruf25 <maruffiardy25@gmail.com>
2023-10-16 00:52:37 +0700

    change plugin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: "brotliCompress",
-      exclude: [/\.(br)$/, /\.(gz)$/],
-      deleteOriginalAssets: true,
-    }),
-  ],
+  plugins: [react()],
 });
